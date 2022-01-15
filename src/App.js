e6440c70d7aaff8bee4de8d90da9e24161570cd0
Miika Kontio <miika.kontio@prime.fi>
2022-01-15 15:49:42 +0200

    Delete task oikeasti nyt poistaa taskin

--- src/App.js
+++ src/App.js
@@ -50,7 +50,13 @@ function App() {
   }
 
   // Delete Task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+
+    await fetch(`http://localhost:5000/tasks/${id}`,
+      {
+        method: 'DELETE'
+      });
+
     console.log('delete', id)
     setTasks(tasks.filter((task) => task.id !== id))
   }
