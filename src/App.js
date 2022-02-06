1329deda1d0085d4ee7cf4fac83f31bb221efeb7
Miika Kontio <miika.kontio@prime.fi>
2022-02-06 10:26:14 +0200

    TaskDetails lisätty

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask'
 import About from './components/About'
+import TaskDetails from './components/TaskDetails';
 
 function App() {
 
@@ -117,6 +118,7 @@ function App() {
           }
           />
           <Route path='/about' element={<About />} />
+          <Route path='/task/:id' element={<TaskDetails />} />
         </Routes>
         <Footer />
       </div>  
