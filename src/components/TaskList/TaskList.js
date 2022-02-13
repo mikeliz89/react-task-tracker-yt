9a11f734daf1f8c02fe32c779c65d9d2d90b99c0
Miika Kontio <miika.kontio@prime.fi>
2022-02-13 21:56:19 +0200

    Listoihin FaListAlt ikoni

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,4 +1,4 @@
-import { FaTimes} from 'react-icons/fa'
+import { FaTimes, FaListAlt} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const TaskList = ({taskList, onDelete}) => {
@@ -6,7 +6,11 @@ const TaskList = ({taskList, onDelete}) => {
     return (
         <div className="task">
             <h3>
-            {taskList.title} <FaTimes className="deleteTaskBtn" style={{color:'red', cursor: 'pointer'}} 
+            <span>
+            <FaListAlt style={{color:'gray', cursor: 'pointer', marginRight:'5px', marginBottom: '-3px' }} />
+            {taskList.title} 
+            </span>
+            <FaTimes className="deleteTaskBtn" style={{color:'red', cursor: 'pointer'}} 
             onClick={() => onDelete(taskList.id)} />
             </h3>
             <p>{taskList.description}</p>
