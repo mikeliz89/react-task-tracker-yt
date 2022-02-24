0621b6c22162e714b217f0055d09ecf7f1ace598
Miika Kontio <miikako89@gmail.com>
2022-02-24 20:22:44 +0200

    firebase config and auth

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database"
+import { getAuth } from "firebase/auth"
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Rakenna firebase/auth
 // TODO: Add SDKs for Firebase products that you want to use
@@ -10,16 +11,17 @@ import { getDatabase } from "firebase/database"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}-default-rtdb.europe-west1.firebasedatabase.app`,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: "959023902950",
-  appId: "1:959023902950:web:efdc553cf4e4a3337a4b27",
-  measurementId: "G-LF64T0FHH3"
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+export const auth = getAuth(app);
 //const analytics = getAnalytics(app);
