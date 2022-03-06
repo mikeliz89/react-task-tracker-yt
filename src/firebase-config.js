e108ae179f992e726b6041ecd50bb1067ccf7f5f
Miika Kontio <miika.kontio@prime.fi>
2022-03-06 10:04:08 +0200

    dataBaseUrl to env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -12,7 +12,7 @@ import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check"
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}-default-rtdb.europe-west1.firebasedatabase.app`,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
