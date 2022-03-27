caeb175d34b7dbad20631f85d0849d5654f9bb1d
Miika Kontio <miikako89@gmail.com>
2022-03-27 17:56:06 +0300

    Nimen ja pituuden lisääminen omassa profiilissa nyt mahdollista. BMICalculatoriin lisätty pituuden haku profiilin tiedoista

--- src/components/BmiCalculator/BmiCalculator.js
+++ src/components/BmiCalculator/BmiCalculator.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 import GoBackButton from '../GoBackButton';
 import { ButtonGroup, Row } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
@@ -6,32 +6,56 @@ import { Form, Table } from 'react-bootstrap';
 import Button from '../Button';
 import { useNavigate } from 'react-router-dom'
 import { db } from '../../firebase-config';
-import { ref, push } from "firebase/database";
+import { ref, push, onValue } from "firebase/database";
 import { useAuth } from '../../contexts/AuthContext';
 import { getCurrentDateAsJson } from '../../utils/DateTimeUtils';
 
 const BmiCalculator = () => {
 
-  const { t } = useTranslation();
-  const navigate = useNavigate();
-  const { currentUser } = useAuth();
+    const { t } = useTranslation();
+    const navigate = useNavigate();
+    const { currentUser } = useAuth();
 
-  //states
-  const [BMI, setBMI] = useState(0)
-  const [weight, setWeight] = useState(0)
-  const [height, setHeight] = useState(0)
+    //states
+    const [BMI, setBMI] = useState(0)
+    const [weight, setWeight] = useState(0)
+    const [height, setHeight] = useState(0)
+    const [profile, setProfile] = useState({})
+
+    //load data
+    useEffect(() => {
+        let isMounted = true;
+        const getProfile = async () => {
+            if(isMounted)
+                await fetchProfileFromFirebase();
+        }
+        getProfile()
+        return () => { isMounted = false };
+    }, [])
+
+    /** Fetch Profile From Firebase */
+    const fetchProfileFromFirebase = async () => {
+        const dbref = ref(db, '/profiles/' + currentUser.uid);
+        onValue(dbref, (snapshot) => {
+            const data = snapshot.val();
+            if(data != null) {
+                setProfile(data);
+                setHeight(data["height"]);
+            }
+        })
+    }
     
-  async function onSubmit(e) {
-    e.preventDefault()
+    async function onSubmit(e) {
+        e.preventDefault()
 
-    let BMITemp = 0;
-    let heightInMeters = height / 100;
-    BMITemp = weight / (heightInMeters * heightInMeters);
-    let BMIRounded = Math.round(BMITemp*10)/10;
-    setBMI(BMIRounded);
+        let BMITemp = 0;
+        let heightInMeters = height / 100;
+        BMITemp = weight / (heightInMeters * heightInMeters);
+        let BMIRounded = Math.round(BMITemp*10)/10;
+        setBMI(BMIRounded);
 
-    saveWeightToFirebase(weight)
-  }
+        saveWeightToFirebase(weight)
+    }
 
     /** Save Given Weight To Firebase for this user */
     const saveWeightToFirebase = async (weight) => {
@@ -40,71 +64,71 @@ const BmiCalculator = () => {
         push(dbref, {weight, currentDateTime});
     }
 
-  return (
-    <div>
-        <Row>
-            <ButtonGroup>
-                <GoBackButton />
-                <Button 
-                  text={t('button_weight_history')}
-                  onClick={() => navigate('/weighthistory')} />
-            </ButtonGroup>
-        </Row>
-        <h3 className="page-title">{t('bmi_calculator_title')}</h3>
-        <Form onSubmit={onSubmit}>
-            <Form.Group className="mb-3" controlId="addTaskFormTaskName">
-                <Form.Label>{t('height')}</Form.Label>
-                <Form.Control type='number' placeholder={t('height')} value={height} onChange={(e) => setHeight(e.target.value)} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="addTaskFormTaskName">
-                <Form.Label>{t('weight')}</Form.Label>
-                <Form.Control type='number' placeholder={t('weight')} value={weight} onChange={(e) => setWeight(e.target.value)} />
-            </Form.Group>
-            <Button type='submit' text={t('calculate_bmi')} className='btn btn-block' />
-        </Form>
-        { BMI > 0 &&
-        <div className="alert alert-primary">
-         {t('your_bmi_is')} : {BMI}
+    return (
+        <div>
+            <Row>
+                <ButtonGroup>
+                    <GoBackButton />
+                    <Button 
+                    text={t('button_weight_history')}
+                    onClick={() => navigate('/weighthistory')} />
+                </ButtonGroup>
+            </Row>
+            <h3 className="page-title">{t('bmi_calculator_title')}</h3>
+            <Form onSubmit={onSubmit}>
+                <Form.Group className="mb-3" controlId="addTaskFormTaskName">
+                    <Form.Label>{t('height')}</Form.Label>
+                    <Form.Control type='number' placeholder={t('height')} value={height} onChange={(e) => setHeight(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="addTaskFormTaskName">
+                    <Form.Label>{t('weight')}</Form.Label>
+                    <Form.Control type='number' placeholder={t('weight')} value={weight} onChange={(e) => setWeight(e.target.value)} />
+                </Form.Group>
+                <Button type='submit' text={t('calculate_bmi')} className='btn btn-block' />
+            </Form>
+            { BMI > 0 &&
+            <div className="alert alert-primary">
+            {t('your_bmi_is')} : {BMI}
+            </div>
+            }
+            <Table>
+                <tbody>
+                <tr>
+                    <td>{t('bmi_1')}</td>
+                    <td>0 - 14,9</td>
+                </tr>
+                <tr>
+                    <td>{t('bmi_2')}</td>
+                    <td>15 - 17,9</td>
+                </tr>
+                <tr>
+                    <td>{t('bmi_3')}</td>
+                    <td>18 - 18,9</td>
+                </tr>
+                <tr>
+                    <td>{t('bmi_4')}</td>
+                    <td>19 - 24,9</td>
+                </tr>
+                <tr>
+                    <td>{t('bmi_5')}</td>
+                    <td>25,0 - 29,9</td>
+                </tr>
+                <tr>
+                    <td>{t('bmi_6')}</td>
+                    <td>30,0 - 34,9</td>
+                </tr>
+                <tr>
+                    <td>{t('bmi_7')}</td>
+                    <td>35,0 - 39,9</td>
+                </tr>
+                <tr>
+                    <td>{t('bmi_8')}</td>
+                    <td>40,0 {t('bmi_ormore')}</td>
+                </tr>
+                </tbody>
+            </Table>
         </div>
-        }
-        <Table>
-            <tbody>
-            <tr>
-                <td>{t('bmi_1')}</td>
-                <td>0 - 14,9</td>
-            </tr>
-            <tr>
-                <td>{t('bmi_2')}</td>
-                <td>15 - 17,9</td>
-            </tr>
-            <tr>
-                <td>{t('bmi_3')}</td>
-                <td>18 - 18,9</td>
-            </tr>
-            <tr>
-                <td>{t('bmi_4')}</td>
-                <td>19 - 24,9</td>
-            </tr>
-            <tr>
-                <td>{t('bmi_5')}</td>
-                <td>25,0 - 29,9</td>
-            </tr>
-            <tr>
-                <td>{t('bmi_6')}</td>
-                <td>30,0 - 34,9</td>
-            </tr>
-            <tr>
-                <td>{t('bmi_7')}</td>
-                <td>35,0 - 39,9</td>
-            </tr>
-            <tr>
-                <td>{t('bmi_8')}</td>
-                <td>40,0 {t('bmi_ormore')}</td>
-            </tr>
-            </tbody>
-        </Table>
-    </div>
-  )
+    )
 }
 
 export default BmiCalculator
--- src/components/MyProfile/ManageMyProfile.js
+++ src/components/MyProfile/ManageMyProfile.js
@@ -1,15 +1,82 @@
 import GoBackButton from '../GoBackButton';
 import { useTranslation } from 'react-i18next';
+import { useAuth } from '../../contexts/AuthContext';
+import { Form } from 'react-bootstrap';
+import { useState, useEffect } from 'react'
+import Button from '../Button';
+import { db } from '../../firebase-config';
+import { ref, onValue, update } from "firebase/database";
+import { getCurrentDateAsJson } from '../../utils/DateTimeUtils';
 
 export default function ManageMyProfile() {
-    
+
     const { t } = useTranslation();
+    const { currentUser } = useAuth();
+
+    //states
+    const [name, setName] = useState('')
+    const [height, setHeight] = useState(0)
+
+    //load data
+    useEffect(() => {
+        let isMounted = true;
+        const getProfile = async () => {
+            if(isMounted)
+                await fetchProfileFromFirebase();
+        }
+        getProfile()
+        return () => { isMounted = false };
+    }, [])
+
+    /** Fetch Profile From Firebase */
+    const fetchProfileFromFirebase = async () => {
+        const dbref = ref(db, '/profiles/' + currentUser.uid);
+        onValue(dbref, (snapshot) => {
+            const data = snapshot.val();
+            if(data != null) {
+                setName(data["name"]);
+                setHeight(data["height"]);
+            }
+        })
+    }
+    
+    /** Recipe Form Submit */
+    const onSubmit = (e) => {
+        e.preventDefault()
+        saveProfileToFirebase()
+    }
+
+    const saveProfileToFirebase = async () => {
+        let data = { name, height};
+        //save edited profile to firebase
+        const updates = {};
+        data["modified"] = getCurrentDateAsJson()
+        updates[`/profiles/${currentUser.uid}`] = data;
+        update(ref(db), updates);
+    }
 
     return (
         <div> 
             <GoBackButton />
             <h3 className="page-title">{t('manage_myprofile_title')}</h3>
-            <p className="text-center">{t('manage_exercises_coming_soon')}</p>
+            <Form onSubmit={onSubmit}>
+                <Form.Group className="mb-3" controlId="myProfileFormName">
+                    <Form.Label>{t('manage_myprofile_name')}</Form.Label>
+                    <Form.Control type='text'
+                        placeholder={ t('manage_myprofile_name')} 
+                        value={name}
+                        onChange={(e) => setName(e.target.value)} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="addIncredientFormAmount">
+                    <Form.Label>{t('manage_myprofile_height')}</Form.Label>
+                    <Form.Control 
+                        type='number' 
+                        placeholder={t('manage_myprofile_height')} 
+                        value={height} 
+                        onChange={(e) => setHeight(e.target.value)} />
+                </Form.Group>
+                <Button type='submit' text={t('manage_myprofile_savebutton')} className='btn btn-primary btn-block' />
+            </Form>
         </div>
     )
 }
