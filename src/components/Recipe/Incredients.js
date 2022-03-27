8fd039020336f78035cd74eef812e58730d1c408
Miika Kontio <miikako89@gmail.com>
2022-03-27 11:22:17 +0300

    ainesosien fonttikokoja pienemmälle jotta sopii paremmin mobiilikäyttöön. Oletuskieleksi fi koko sovellukseen. Ainesosat otsikon kielitermi

--- src/components/Recipe/Incredients.js
+++ src/components/Recipe/Incredients.js
@@ -1,21 +1,25 @@
 import React from 'react'
 import { FaTimes} from 'react-icons/fa'
+import { useTranslation } from 'react-i18next';
 
 export default function Incredients({incredients, recipeID, onDelete}) {
+
+  const { t } = useTranslation();
+
   return (
       <>
-        <h3>Ainesosat</h3>
+        <h4>{ t('incredients_header')}</h4>
         {incredients 
           ? incredients.map((incredient, index) =>
-          <div>
-            <h5>
+          <div key={incredient.id}>
+            <p>
             {incredient.name} - {incredient.amount} {incredient.unit}
                 {  
             <> <FaTimes className="deleteTaskBtn" style={{color:'red', cursor: 'pointer'}} 
                 onClick={() => onDelete(recipeID, incredient.id)} />
             </>
             }
-            </h5>
+            </p>
         </div>
         ) : ''
         }
