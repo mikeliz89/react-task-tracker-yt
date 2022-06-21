1889ac65214143f7a4e642c24da35f624fbfcb46
Miika Kontio <miikako89@gmail.com>
2022-06-21 18:44:44 +0300

    refactorings

--- src/components/GoBackButton.js
+++ src/components/GoBackButton.js
@@ -1,10 +1,17 @@
+//react
 import { useNavigate } from 'react-router-dom';
-import Button from '../components/Button'
 import { useTranslation } from 'react-i18next';
+//buttons
+import Button from '../components/Button';
 
 const GoBackButton = () => {
+
+  //navigation
   const navigate = useNavigate();
+
+  //translation
   const { t } = useTranslation('common', { keyPrefix: 'common.buttons' });
+
   return (
     <Button text={''} showIconArrowLeft={true} onClick={() => navigate(-1)} />
   )
