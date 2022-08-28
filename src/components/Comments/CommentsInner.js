1c83a6a5fd8d9181f18a77eb5d61ef8cea3196cd
Miika Kontio <miikako89@gmail.com>
2022-08-28 17:52:40 +0300

    space

--- src/components/Comments/CommentsInner.js
+++ src/components/Comments/CommentsInner.js
@@ -20,6 +20,7 @@ const CommentsInner = ({ comments, onDelete }) => {
                         <p>
                             {getJsonAsDateTimeString(comment.created, i18n.language)} <br />
                             {comment.createdBy}: {comment.text}
+                            &nbsp;
                             <Icon name='times' className="deleteBtn" style={{ color: 'red', cursor: 'pointer', fontSize: '1.2em' }}
                                 onClick={() => { if (window.confirm(t('delete_comment_confirm'))) { onDelete(comment.id); } }} />
                         </p>
