9042d1c9e0b5162ff756c319da879886f15275f4
mikeliz89 <miika.kontio@prime.fi>
2026-07-12 22:09:36 +0300

    footer näyttää nyt jatkossa kokoajan kuluvan vuoden

--- src/components/Site/Footer.js
+++ src/components/Site/Footer.js
@@ -6,11 +6,12 @@ import NavButton from '../Buttons/NavButton';
 export default function Footer() {
 
     const { t } = useTranslation(TRANSLATION.TRANSLATION, { keyPrefix: TRANSLATION.FOOTER })
+    const currentYear = new Date().getFullYear();
 
     return (
         <footer>
             <NavButton to={NAVIGATION.ABOUT} className=''>
-                {t('copyright')} &copy; 2025
+                {t('copyright')} &copy; {currentYear}
             </NavButton>
         </footer>
     )
